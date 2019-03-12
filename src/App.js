05daf0bd96e31fae274c9010f08b3f9244b2e4fb
Jscholar <jinruncai@gmail.com>
2019-03-12 16:44:18 -0700

    Added routing package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import {BrowserRouter} from 'react-router-dom'
+
+import Main from './containers/Main/Main'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <BrowserRouter>
+          <Main />
+        </BrowserRouter>
       </div>
     );
   }
 }
 
-export default App;
+export default App
