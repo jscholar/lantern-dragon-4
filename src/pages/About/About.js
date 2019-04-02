3a4fead7ed9ca0b1e36e1141e9d5cc0b3c670400
jscholar <jinruncai@gmail.com>
2019-04-02 12:19:18 -0700

    Added site summaryi

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import PROFILES from './Profiles';
+
+import _profiles from './profiles';
+import summary from './summary';
+import classes from './About.module.css';
 
 const About = () => {
-    const profiles = PROFILES.map((profile) => {
+    const profiles = _profiles.map((profile) => {
         return (
             <div>
-                <p>{`${profile.name}: ${profile.desc}
-                `}</p>
+                <p>{`${profile.name}: ${profile.desc}`}</p>
             </div>
         )
     })
     return (
         <div>
+            <div className={classes.Summary}>{summary}</div>
             {profiles}
         </div>
     )
---
+++ src/pages/About/summary.js
@@ -0,0 +1,15 @@
+const summary = `
+Welcome.
+
+Lantern Dragon Works is a small collection of fictional novels inspired by Japanese visual novels and themes.
+
+For now, this website is still in its infancy, as are my development skills.Therefore, if you
+would like a more complete experience, on a more worthy interface, please visit our WordPress Page.
+Though be sure to come back and visit again when this project becomes awesome.
+
+Regards, and enjoy your stay,
+
+A lowly member of the Lantern Dragons Works team.
+`;
+
+export default summary;
