460604f7ca734814d377d82c3a4ca390059f6373
jscholar <jinruncai@gmail.com>
2019-07-14 13:54:20 -0700

    Profile cards on Credits Page

--- src/pages/Credits/Credits.js
+++ src/pages/Credits/Credits.js
@@ -2,18 +2,30 @@ import React from 'react';
 
 import _profiles from './profiles';
 
+import classes from './Credits.module.css'
+
 // <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 
 const Credits = () => {
     const profiles = _profiles.map((profile) => {
         return (
-            <div>
-                <p>{`${profile.name}: ${profile.desc}`}</p>
+            <div key={profile.id} className={classes.Profile}>
+                <div className={classes.Frame}>
+                    <div className={classes.Name}>
+                        <span>{profile.name}</span>
+                    </div>
+                    <div className={classes.Picture}>
+                        <img className={classes.Portrait} src={`img/${profile.id}.jpg`} alt={profile.name}></img>
+                    </div>
+                </div>
+                <div className={classes.Description}>
+                    <p>{`${profile.name}: ${profile.desc}`}</p>
+                </div>
             </div>
         )
     })
     return (
-        <div>
+        <div className={classes.Credits}>
             {profiles}
         </div>
     )
--- src/pages/Credits/profiles.js
+++ src/pages/Credits/profiles.js
@@ -1,17 +1,21 @@
 const profiles = [
     {
+        "id": "delta0G",
         "name": "delta0G",
         "desc": "Math major who spent way too much time working on this story instead of doing actual math."
     },
     {
+        "id": "Jacque",
         "name": "Jacque the 'Rock'",
         "desc": "Master of doodles and story artist. When asked to do pictures, the response was “F**k my life, bro”."
     },
     {
+        "id": "CP3",
         "name": "Computer Three",
         "desc": "Practically the only person that ever read the drafts of the story.  Once in a blue moon editor and scarce proofreader. Also author of Masks."
     },
     {
+        "id": "Icarus",
         "name": "Icarus",
         "desc": "Fan of A Dragon’s Revolt and website designer hobbyist, designer of the future Lantern Dragon Work site. Like Computer Three, seldom contributor and proofreader.",
         "status": "MVP"
